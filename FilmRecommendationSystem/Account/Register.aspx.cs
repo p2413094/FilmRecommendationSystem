27d53157bed3c26079fb6512c53ff3ad419c33a6
Rajesh Dhooper <P2413094@my365.dmu.ac.uk>
2021-01-15 13:11:09 +0000

    Added reference to Classes in other projects

--- FilmRecommendationSystem/Account/Register.aspx.cs
+++ FilmRecommendationSystem/Account/Register.aspx.cs
@@ -30,6 +30,7 @@ namespace FilmRecommendationSystem.Account
             else 
             {
                 ErrorMessage.Text = result.Errors.FirstOrDefault();
+                
             }
         }
     }
