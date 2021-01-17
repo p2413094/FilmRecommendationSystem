4585f90ff27d42618e06fe2fc9a6406f7a002cef
Rajesh Dhooper <P2413094@my365.dmu.ac.uk>
2021-01-17 13:19:20 +0000

    Added testLink and added tests

---
+++ Classes/clsLink.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Classes
+{
+    public class clsLink
+    {
+        public int FilmId { get; set; }
+        public string ImdbId { get; set; }
+
+
+        public string Valid(string imdbId)
+        {
+            string error = "";
+            if (imdbId.Length > 8)
+            {
+                error = "The ImdbId must not be more than 8 characters";
+            }
+            if (imdbId.Length == 0)
+            {
+                error = "The ImdbId must be more than 1 character";
+            }
+            return error;
+        }
+    }
+}
---
+++ Testing/tstLink.cs
@@ -0,0 +1,126 @@
+﻿using Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing
+{
+    [TestClass]
+    public class tstLink
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            clsLink aLink = new clsLink();
+            Assert.IsNotNull(aLink);
+        }
+
+        [TestMethod]
+        public void FilmIdPropertyOk()
+        {
+            clsLink aLink = new clsLink();
+            int filmId = 1;
+            aLink.FilmId = filmId;
+            Assert.AreEqual(aLink.FilmId, filmId);
+        }
+
+        [TestMethod]
+        public void ImdbIdPropertyOk()
+        {
+            clsLink aLink = new clsLink();
+            string imdbId = "0072271";
+            aLink.ImdbId = imdbId;
+            Assert.AreEqual(aLink.ImdbId, imdbId);
+        }
+
+        [TestMethod]
+        public void ValidMethodOk()
+        {
+            clsLink aLink = new clsLink();
+            string error = "";
+            string imdbId = "0072271";
+            error = aLink.Valid(imdbId);
+            Assert.AreEqual(error, "");
+        }
+
+        [TestMethod]
+        public void ImdbIdLessOne()
+        {
+            clsLink aLink = new clsLink();
+            string error = "";
+            string imdbId = "";
+            error = aLink.Valid(imdbId);
+            Assert.AreNotEqual(error, "");
+        }
+
+        [TestMethod]
+        public void ImdbIdMinBoundary()
+        {
+            clsLink aLink = new clsLink();
+            string error = "";
+            string imdbId = "0";
+            error = aLink.Valid(imdbId);
+            Assert.AreEqual(error, "");
+        }
+
+        [TestMethod]
+        public void ImdbIdMinPlusOne()
+        {
+            clsLink aLink = new clsLink();
+            string error = "";
+            string imdbId = "00";
+            error = aLink.Valid(imdbId);
+            Assert.AreEqual(error, "");
+        }
+
+        [TestMethod]
+        public void ImdbIdMid()
+        {
+            clsLink aLink = new clsLink();
+            string error = "";
+            string imdbId = "0000";
+            error = aLink.Valid(imdbId);
+            Assert.AreEqual(error, "");
+        }
+
+        [TestMethod]
+        public void ImdbIdMaxMinusOne()
+        {
+            clsLink aLink = new clsLink();
+            string error = "";
+            string imdbId = "0000000";
+            error = aLink.Valid(imdbId);
+            Assert.AreEqual(error, "");
+        }
+
+        [TestMethod]
+        public void ImdbIdMaxBoundary()
+        {
+            clsLink aLink = new clsLink();
+            string error = "";
+            string imdbId = "00000000";
+            error = aLink.Valid(imdbId);
+            Assert.AreEqual(error, "");
+        }
+
+        [TestMethod]
+        public void ImdbIdMaxPlusOne()
+        {
+            clsLink aLink = new clsLink();
+            string error = "";
+            string imdbId = "000000000";
+            error = aLink.Valid(imdbId);
+            Assert.AreNotEqual(error, "");
+        }
+
+        [TestMethod]
+        public void ImdbIdExtremeMax()
+        {
+            clsLink aLink = new clsLink();
+            string error = "";
+            string imdbId = "";
+            imdbId = imdbId.PadRight(80, '0');
+            error = aLink.Valid(imdbId);
+            Assert.AreNotEqual(error, "");
+        }
+    }
+}
