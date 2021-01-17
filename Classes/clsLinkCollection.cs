02381db7b87958e927889b49ffd86178f9ca82a3
Rajesh Dhooper <P2413094@my365.dmu.ac.uk>
2021-01-17 17:24:35 +0000

    Added tstLinkCollection and removed ConnectionStrings.config

---
+++ Classes/clsLinkCollection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Classes;
+
+namespace Classes
+{
+    public class clsLinkCollection
+    {
+        private List<clsLink> mAllLinks = new List<clsLink>();
+
+        public int Count
+        {
+            get {return mAllLinks.Count;}
+            set {}
+        }
+
+        public List<clsLink> AllLinks
+        {
+            get {return mAllLinks;}
+            set {mAllLinks = value;}
+        }
+
+        public clsLinkCollection()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblLinks_SelectAll");
+            Int32 recordCount = DB.Count;
+            Int32 index = 0;
+            while (index < recordCount)
+            {
+                clsLink aLink = new clsLink();
+                aLink.FilmId = Convert.ToInt32(DB.DataTable.Rows[index]["FilmId"]);
+                aLink.ImdbId = DB.DataTable.Rows[index]["ImdbId"].ToString();
+                mAllLinks.Add(aLink);
+                index++;
+            }
+        }
+    }
+}
---
+++ Testing/tstLinkCollection.cs
@@ -0,0 +1,55 @@
+﻿using Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Testing
+{
+    [TestClass]
+    public class tstLinkCollection
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            clsLinkCollection AllLinks = new clsLinkCollection();
+            Assert.IsNotNull(AllLinks);
+        }
+
+        [TestMethod]
+        public void CountPropertyOk()
+        {
+            clsLinkCollection AllLinks = new clsLinkCollection();
+            Int32 someCount = 193609;
+            AllLinks.Count = someCount;
+            Assert.AreEqual(AllLinks.Count, someCount);
+        }
+
+        [TestMethod]
+        public void AllLinksOk()
+        {
+            clsLinkCollection Links = new clsLinkCollection();
+            List<clsLink> testList = new List<clsLink>();
+            clsLink testItem = new clsLink();
+            testItem.FilmId = 1;
+            testItem.ImdbId = "000000";
+            testList.Add(testItem);
+            Links.AllLinks = testList;
+            Assert.AreEqual(Links.AllLinks, testList);
+        }
+
+        [TestMethod]
+        public void CountMatchesList()
+        {
+            clsLinkCollection Links = new clsLinkCollection();
+            List<clsLink> testList = new List<clsLink>();
+            clsLink testItem = new clsLink();
+            testItem.FilmId = 1;
+            testItem.ImdbId = "00000";
+            testList.Add(testItem);
+            Links.AllLinks = testList;
+            Assert.AreEqual(Links.Count, testList.Count);
+        }
+
+        
+    }
+}
