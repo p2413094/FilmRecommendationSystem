a82c9a23d525b53a150d4d0e4fd6be091b81bc56
Rajesh Dhooper <P2413094@my365.dmu.ac.uk>
2021-01-23 15:12:29 +0000

    Changed stored procedure for selecting all moods based on description

--- Classes/clsMood.cs
+++ Classes/clsMood.cs
@@ -25,10 +25,8 @@ namespace Classes
             clsDataConnection DB = new clsDataConnection();
             DB.AddParameter("@Description", moodDesc);
             DB.Execute("sproc_tblMood_FilterByDescription");
-            Int32 MoodExists = 0;
-            MoodExists = Convert.ToInt32(DB.DataTable.Rows[0]["ReturnValue"]);
 
-            if (MoodExists != 0)
+            if (DB.Count != 0)
             {
                 error = "This mood already exists";
             }
